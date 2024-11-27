dedcee2b50ddedc7a4526c72bc0ec804377d52ae
Sakura123582 <bienaimejovaniekerry@gmail.com>
2024-11-26 22:15:58 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "50931461936"
-global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
+global.ownernomer = "18093275286"
+global.ownername = "𓄂⍣⃝𝐆𝚯𝐃D𝚵⃝𝐒𝐒✮͢≛⃝𝐊𝚫𝚻𝚮𝚵𝚪𝚰𝚴𝚵𝄟🌹"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America, Haiti, Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
+global.ownernumber = '18093275286'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
@@ -18,7 +18,7 @@ global.author = '𝙎-𝙏𝞢𝞜'
 
 //console view/theme
 global.themeemoji = '🗾'
-global.wm = "𝙎-𝙏𝞢𝞜"
+global.wm = "𓄂⍣⃝𝐆𝚯D𝐃𝚵⃝𝐒𝐒✮͢≛⃝𝐊𝚫𝚻𝚮𝚵𝚪𝚰𝚴𝚵𝄟🌹"
 
 //theme link
 global.link = 'https://whatsapp.com/channel/0029Vakp0UnICVfe3I2Fe72w'
@@ -27,8 +27,8 @@ global.link = 'https://whatsapp.com/channel/0029Vakp0UnICVfe3I2Fe72w'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
